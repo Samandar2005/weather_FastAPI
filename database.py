1c40883ec6c33eaf8878644920b5fe66942822a0
Samandar2005 <samandarboriyev2005@gmail.com>
2024-12-13 20:26:49 +0500

    add requirements and .env file and update database.py

--- database.py
+++ database.py
@@ -1,10 +1,20 @@
 from tortoise.contrib.fastapi import register_tortoise
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 def init_db(app):
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_host = os.getenv("DB_HOST")
+    db_name = os.getenv("DB_NAME")
+
+    db_url = f"postgres://{db_user}:{db_password}@{db_host}/{db_name}"
+
     register_tortoise(
         app,
-        db_url="postgres://samandar:1234@localhost/weather_db",
+        db_url=db_url,
         modules={"models": ["models"]},
         generate_schemas=True,
         add_exception_handlers=True,
